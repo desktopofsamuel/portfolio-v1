30fc1a8b242576977ad8bb03a95b33474e031b2c
Samuel Wong <samuel@playa.hk>
2018-09-29 23:03:38 +0200

    Install GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,5 +109,25 @@ module.exports = {
         name: 'project'
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-114278308-3",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "desktopofsamuel.com",
+      },
+    },
   ],
 }
